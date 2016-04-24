ba4e95bcd9b560ab962b3cc407d3603d7d095f29
cony <cony@huoban.com>
2016-04-25 02:11:54 +0800

    选择分组的标识字段

--- content_script.js
+++ content_script.js
@@ -43,16 +43,7 @@ function add_button() {
         }
 
         if (button.attr("hb_status") == "close") {
-            button.attr("hb_status", "open");
-            button.attr("title", "点击退出脑图模式");
-
-            chrome.runtime.sendMessage({"type":"getApp"}, function(app) {
-                chrome.runtime.sendMessage({"type":"listItem"}, function(itemResult) {
-                    treeData = buildTreeData(app, itemResult);
-                    $("#root").hide();
-                    displayTree(treeData);
-                });
-            });
+            showConfirm();
         } else {
             button.attr("hb_status", "close");
             button.attr("title", "点击退出脑图模式");
@@ -60,6 +51,82 @@ function add_button() {
         }
         return;
     });
+}
+
+var confirmIsCreated = false;
+
+function showConfirm() {
+    var confirmPopup = $('#plugin_huobanmind_confirm');
+    $('div .hb_mask').show();
+    if (confirmIsCreated) {
+        confirmPopup.show();
+        return;
+    }
+
+    chrome.runtime.sendMessage({"type":"getApp"}, function(app) {
+        confirmPopup = $(
+            '<div id="plugin_huobanmind_confirm" '
+                + 'style="'
+                    + 'width: 400px; text-align: center; position: fixed; bottom: 40%; left: 20%; '
+                    + 'z-index: 2147483639; border-style: solid; border-width: thin; border-color: #e2e2d8; '
+                    + 'padding: 10px; background: white; '
+                + '"'
+            + ' >'
+                + '<div '
+                    + 'style="'
+                        + 'font-size: large; padding-bottom: 20px; border-bottom-style: solid; '
+                        + 'border-bottom-width: thin; border-bottom-color: #e2e2d8; '
+                    + '"'
+                + ' >请选择分组字段</div>'
+                + '<select style="margin-top: 10px; margin-bottom: 20px; "></select>'
+                + '<div>'
+                    + '<a id="plugin_huobanmind_confirm_button" '
+                        + 'style="padding: 20px; font-size: 14px; border-color: black; cursor: pointer; "'
+                    + '>确认</a>'
+                    + '<a id="plugin_huobanmind_cancel_button" '
+                        + 'style="padding: 20px; font-size: 14px; border-color: black; cursor: pointer; "'
+                    + '>取消</a>'
+                + '</div>'
+            + '</div>');
+        var allowedFields = listAllowedNodeField(app);
+        var allowedField, option;
+
+        for (var i = allowedFields.length - 1; i >= 0; i--) {
+            allowedField = allowedFields[i];
+            option = $('<option hb_field_id="'+allowedField.field_id+'">'+allowedField.name+'</option>');
+            confirmPopup.children("select").append(option);
+        }
+
+        $('body').append(confirmPopup);
+        confirmIsCreated = true;
+
+        $('#plugin_huobanmind_cancel_button').click(function() {
+            $('div .hb_mask').hide();
+            confirmPopup.hide();
+        });
+
+        $('#plugin_huobanmind_confirm_button').click(function() {
+            var selectedOption = $('#plugin_huobanmind_confirm select').get(0).selectedOptions[0];
+            var groupByFieldId = parseInt($(selectedOption).attr('hb_field_id'));
+            clickConfirmEvent(app, groupByFieldId);
+
+            $('div .hb_mask').hide();
+            confirmPopup.hide();
+        })
+    });
+}
+
+function clickConfirmEvent(app, groupByFieldId) {
+    var button = $('#plugin_huobanmind_button');
+    button.attr("hb_status", "open");
+    button.attr("title", "点击退出脑图模式");
+
+    chrome.runtime.sendMessage({"type":"listItem"}, function(itemResult) {
+        treeData = buildTreeData(app, groupByFieldId, itemResult);
+        $("#root").hide();
+        displayTree(treeData);
+    });
+
 }
 
 function check_in_list() {
